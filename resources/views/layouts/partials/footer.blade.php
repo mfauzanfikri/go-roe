34a1b3b7a2bb55fdf941d1675ca13a1483a879c8
mfauzanfikri <mfauzanfikri09@gmail.com>
2025-06-28 14:15:54 +0700

    init

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer id="footer" class="footer">
+    <div class="container footer-top">
+        {{-- All original footer content from your HTML goes here --}}
+    </div>
+
+    <div class="container copyright text-center mt-4">
+        <p>&copy; <span>Copyright</span> <strong class="px-1 sitename">Restaurantly</strong> <span>All Rights Reserved</span></p>
+        <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer>
